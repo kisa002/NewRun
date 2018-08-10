b250133e19b344057535d4d13e08fc58d7a32c6e
kisa002 <vnycall74@naver.com>
2018-08-11 06:10:34 +0900

    멀티 구현

---
+++ Assets/Scripts/HolyKnight/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public Vector2 prevMouse, currentMouse;
+
+    public float angle, distance;
+	
+	void Update ()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+            TouchIn();
+        else if (Input.GetMouseButtonUp(0))
+            TouchOut();
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector2.down * .05f);
+	}
+
+    public void TouchIn()
+    {
+        prevMouse = Input.mousePosition;
+    }
+
+    public void TouchOut()
+    {
+        currentMouse = Input.mousePosition;
+
+        rigidbody2D.AddForce(prevMouse - currentMouse);
+
+        distance = Vector2.Distance(prevMouse, currentMouse);
+        angle = Vector2.Angle(prevMouse, currentMouse);
+
+        //player.transform.eulerAngles = new Vector3(0, 0, angle);
+
+        //player.transform.Translate(Vector2.up * 1);
+        
+        Debug.Log("Distance: " + distance.ToString());
+        Debug.Log("Angle: " + angle.ToString());
+    }
+}
